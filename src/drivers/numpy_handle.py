777c79ea7d5612e71ab1716ec543905054791946
André Gicoski <andregicoskisz@gmail.com>
2025-02-18 12:14:35 -0300

    creating calc2 and injecting dependences

--- src/drivers/numpy_handle.py
+++
@@ -1,9 +0,0 @@
-import numpy
-from typing import List
-
-class NumpyHandle():
-    def __init__(self) -> None:
-        self.__np = numpy
-
-    def standart_derivation(self, numbers: List[float]) -> float:
-        return self.__np.std(numbers)
